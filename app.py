430ff767d337c633f7407a59de3da25156866175
neuro <kurpanagis@gmail.com>
2022-01-29 14:49:52 +0200

    Oh shit I broke production Version2 ¯\_(⊙︿⊙)_/¯

---
+++ app.py
@@ -0,0 +1,42 @@
+import os
+import re
+from tkinter import Frame
+import cv2
+import numpy as np
+from tqdm import *
+import matplotlib.pyplot as plt
+from time import sleep
+####
+# get file names of frames
+
+vc=cv2.VideoCapture('/dev/video2')#0)
+#
+
+######
+while(1):
+    return_value, image = vc.read()
+    # create a zero array
+    stencil =np.zeros_like(image[:,:,0])
+    # specify coordinates of the polygon
+    polygon = np.array([[50,270], [220,160], [360,160], [480,270]])
+    # fill polygon with ones
+    cv2.fillConvexPoly(stencil, polygon, 1)
+    img = cv2.bitwise_and(image[:,:,0], image[:,:,0], mask=stencil)
+    ret, thresh = cv2.threshold(img, 130, 145, cv2.THRESH_BINARY)
+    # plot image
+    lines = cv2.HoughLinesP(thresh, 1, np.pi/180, 30, maxLineGap=200)
+
+    # create a copy of the original image
+    dmy =image[:,:,0].copy()
+    # draw Hough lines
+    try:
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            cv2.line(dmy, (x1, y1), (x2, y2), (255, 0, 0), 3)
+        print("YES")
+    except:
+        print("No")
+    # plot image
+    #plt.figure(figsize=(10,10))
+    #plt.imshow(dmy, cmap= "gray")
+    #plt.show()
